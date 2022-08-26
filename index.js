d1ec3c23bb1c7a7837d29658787eac3a34ea3d48
Tess <tere0506@stud.kea.dk>
2022-08-26 15:01:11 +0200

    initial_comit

---
+++ index.js
@@ -0,0 +1,12 @@
+//Setting up
+//Click "Calculate"
+//Read first number ´
+//Read second number
+//Read operator
+//Calculat (+, -, %, *)
+//Check if rounded clicked
+//If Yes the Round (If no skip)
+//Write result in first input field
+//Append result to list
+//Scroll list to last 4 results
+//Clickable again
